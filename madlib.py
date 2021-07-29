2f857b332d773d4699fb83cde79aa38914d9d701
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2021-07-29 05:05:38 +0000

    first commit

---
+++ madlib.py
@@ -0,0 +1,9 @@
+adj = input("adjective: ")
+verb1 = input("Verb: ") 
+verb2 = input("Verb: ")
+famous_person = input("famous person: ")
+
+madlib = f"full statck development is so {adj}!  It makes me excited all the time because \
+I love to {verb1}.  Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
